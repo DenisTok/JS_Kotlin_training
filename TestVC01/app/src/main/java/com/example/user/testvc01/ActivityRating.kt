facea0159e8bd0aada5721c0be323b8bb3151177
Tokarev Denis <denis.q5@mail.ru>
2018-09-23 10:59:09 +0300

    всё

---
+++ TestVC01/app/src/main/java/com/example/user/testvc01/ActivityRating.kt
@@ -0,0 +1,12 @@
+package com.example.user.testvc01
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityRating : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rating)
+    }
+}
